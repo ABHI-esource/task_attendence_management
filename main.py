e65742e3a7729b7ee543f4536b233f45e48bc78c
ashokpatil0340-blip <ashokpatil0340@gmail.com>
2026-04-22 12:32:11 +0530

    abhi

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def Hello_World():
+    return render_template("Home.html")
+
+@app.route("/about")
+def about():
+    return render_template("About.html")
+
+@app.route("/signup")
+def signup():
+    return render_template ("Signup.html")
+
+@app.route("/login")
+def login():
+    return render_template ("Login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
